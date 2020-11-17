6af8a4d990373b37df1e51e621c7de86ea77b5ef
ritzenthaler.audrey@gmail.com <ritzenthaler.audrey@gmail.com>
2020-11-17 10:51:13 +0100

    init project

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import './App.css'
+import Student from './components/Student'
+import Teacher from './components/Teacher'
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Student />
+      <Teacher />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
