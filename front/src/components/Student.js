3f1700757c8f28800d117e48e1019812862bfc65
SebG-prog <sebastien-gruet@orange.fr>
2021-03-03 11:15:52 +0100

    Erased previous code to prepare the React Native App

--- front/src/components/Student.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Student = () => {
-  return <div>Student</div>
-}
-
-export default Student
--- front/src/components/Teacher.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const Teacher = () => {
-  return <div>Prof</div>
-}
-
-export default Teacher
