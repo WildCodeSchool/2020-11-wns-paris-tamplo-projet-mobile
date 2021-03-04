2d56a60df44754f1cc8f7621048b8f01f74da9d8
SebG-prog <sebastien-gruet@orange.fr>
2021-03-04 12:37:35 +0100

    test

--- components/organismes/QuestionAnswer.js
+++ components/organismes/QuestionAnswer.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Button, ScrollView, Text, View } from "react-native";
 import CheckBox from "@react-native-community/checkbox";
 import Container from "../templates/Container";
 
@@ -23,14 +23,20 @@ const QuestionAnswer = ({ question }) => {
         <Text> {question.question} </Text>
         {question &&
           question.responses.map((el, index) => {
+            const isSelected = answer.includes(el.id) ? "YES" : "NO";
+            console.log(isSelected);
             return (
               <View key={el.id}>
                 <Text>{el.response}</Text>
-                <CheckBox
+                {/* <CheckBox
                   disabled={false}
                   value={answer.includes(el.id)}
                   onChange={() => handleChange(el.id)}
                   onValueChange={(newValue) => console.log(newValue)}
+                /> */}
+                <Button
+                  onPress={() => handleChange(el.id)}
+                  title={isSelected}
                 />
               </View>
             );
